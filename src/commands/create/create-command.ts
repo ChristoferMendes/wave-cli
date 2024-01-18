fb0c68b0803f69763ef6e97889dfde2cbe966897
Christofer <christoferluizdsm@gmail.com>
2024-01-17 22:44:55 -0300

    refactor: rename new-command.ts and create-files-or-directories.ts

--- src/commands/create/create-command.ts
+++ src/commands/create/create-command.ts
@@ -5,19 +5,22 @@ import { join } from "path";
 export default {
   description: "Initialize a new project",
   run: async (options) => {
-    const { compileTemplate } = options; 
+    const { compileTemplate, args } = options; 
+    const [commandName, commandDescription] = args.argsArray
 
     const filesStructure: FilesStructure = {
       src: {
-        commands: ['help.ts']
+        commands: [`${commandName}.ts`]
       }
     }
 
     createFilesOrDirectories(filesStructure);
 
-    const helpFilePath = join(process.cwd(), 'src/commands/help.ts');
-    const helpTemplatePath = join(process.cwd(), 'src/templates/help.surf');
-    const result = await compileTemplate(helpTemplatePath, {});
+    const helpFilePath = join(process.cwd(), `src/commands/${commandName}.ts`);
+    const helpTemplatePath = join(process.cwd(), 'src/templates/command.surf');
+    const result = await compileTemplate(helpTemplatePath, {
+      description: commandDescription
+    });
     Bun.write(helpFilePath, result);
   },
 } as WaveCommand;
