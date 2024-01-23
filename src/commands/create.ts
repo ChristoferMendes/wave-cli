40fd9d7c769c412a8653789036e3546370e9f350
Christofer <christoferluizdsm@gmail.com>
2024-01-23 08:45:39 -0300

    feat(create): Initialize a new project and create necessary files

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,27 +1,59 @@
 import { WaveCommand } from "wave-shell";
 import { join } from "path";
-import { FilesStructure, createFileStructure } from "../utils/create-file-structure";
+import {
+  FilesStructure,
+  createFileStructure,
+} from "../utils/create-file-structure";
+import { z } from "zod";
 
 export default {
   description: "Initialize a new project",
-  run: async (options) => {
-    const { compileTemplate, args } = options; 
-    const [commandName, commandDescription] = args.argsArray
+  validateArgs: () => {
+    return {
+      argsArraySchema: z
+        .array(
+          z.string().refine((data) => isNaN(Number(data)), {
+            message: "Input should be a string and not a number.",
+          })
+        )
+    };
+  },
+  run: async ({ args, compileTemplate, prompt, print }) => {
+    const [name] = args.argsArray;
+    let projectName = name;
 
-    const filesStructure: FilesStructure = {
-      src: {
-        commands: [`${commandName}.ts`]
-      }
+    if (!name) {
+      projectName = await prompt.ask("What is the name of your project?");
     }
 
+    const filesStructure: FilesStructure = {
+      bin: [projectName],
+      src: {
+        commands: [`${projectName}.ts`],
+      },
+    };
     createFileStructure(filesStructure);
 
-    const commandTemplatePath = join(process.cwd(), 'src/templates/command.surf');
-    const parsedTemplate = await compileTemplate(commandTemplatePath, {
-      description: commandDescription
+    const creationTemplatePath = join(process.cwd(), "src/templates/create/");
+
+    const bin = await compileTemplate(join(creationTemplatePath, "bin.surf"), {
+      projectName,
     });
 
-    const commandFilePath = join(process.cwd(), `src/commands/${commandName}.ts`);
-    Bun.write(commandFilePath, parsedTemplate);
+    const command = await compileTemplate(
+      join(creationTemplatePath, "hello-world.surf"),
+      {
+        description: "Hello World command",
+      }
+    );
+
+    Bun.write(join(process.cwd(), "bin", projectName), bin);
+    Bun.write(
+      join(process.cwd(), "src", "commands", `${projectName}.ts`),
+      command
+    );
+
+    print.success(`Project ${projectName} created successfully!`);
+
   },
 } as WaveCommand;
