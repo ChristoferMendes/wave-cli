47bcaea202399370d49601135739cfc3fec67a68
Christofer <christoferluizdsm@gmail.com>
2024-01-23 08:45:46 -0300

    feat(commands): add new command file

---
+++ src/commands/new.ts
@@ -0,0 +1,38 @@
+import { WaveCommand } from "wave-shell";
+import { z } from "zod";
+import { join } from "path";
+import { createFileStructure } from "../utils/create-file-structure";
+
+export default {
+  validateArgs: () => {
+    return {
+      argsArraySchema: z
+        .array(
+          z.string().refine((data) => isNaN(Number(data)), {
+            message: "Input should be a string and not a number.",
+          })
+        )
+        .nonempty(),
+    }
+  },
+  async run({ args, compileTemplate }) {
+    const [name, description] = args.argsArray
+
+    const templatesFolder = join(process.cwd(), "src/templates/")
+
+    const filePath = join(templatesFolder, "command.surf")
+
+    createFileStructure({
+      src: {
+        commands: [`${name}.ts`]
+      }
+    })
+
+    const result = await compileTemplate(filePath, {
+      description,
+      name
+    })
+    
+    Bun.write(join(process.cwd(), "src", "commands", `${name}.ts`), result)
+  },
+} as WaveCommand
