4c587c43ec232af1c26cdede9e450d4b693ed1d6
ChristoferMendes <christoferluizdsm@gmail.com>
2024-01-29 00:38:39 -0300

    feat(create): add project install steps

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,10 +1,75 @@
 import { WaveCommand } from "wave-shell";
 import { join } from "path";
-import {
-  FilesStructure,
-  createFileStructure,
-} from "../utils/create-file-structure";
 import { z } from "zod";
+import { $ } from 'bun';
+import { FilesStructure, createFileStructure } from "~/utils/create-file-structure";
+
+const root = join(__dirname, '..', '..');
+
+async function getProjectName(prompt: any, args: any) {
+  const [name] = args.argsArray;
+  if (!name) {
+    const response = await prompt.ask("What is the name of your project?");
+    return response.value;
+  }
+  return name;
+}
+
+async function createProjectStructure(projectName: string) {
+  const filesStructure: FilesStructure = {
+    [projectName]: {
+      bin: [projectName],
+      src: {
+        commands: [`${projectName}.ts`],
+      },
+    },
+  };
+  createFileStructure(filesStructure);
+}
+
+async function compileTemplates(projectName: string, compileTemplate: (filePath: string, data: Record<string, any>) => Promise<string>) {
+  const creationTemplatePath = join(root, "src/templates/create/");
+
+  const bin = await compileTemplate(join(creationTemplatePath, "bin.surf"), {
+    projectName,
+  });
+
+  const command = await compileTemplate(
+    join(creationTemplatePath, "hello-world.surf"),
+    {
+      description: "Hello World command",
+    }
+  );
+
+  return { bin, command };
+}
+
+async function writeTemplates(projectName: string, bin: string, command: string) {
+  $`cd ${projectName} && bun write bin/${projectName} ${bin}`;
+  $`cd ${projectName} && bun write src/commands/${projectName}.ts ${command}`;
+}
+
+async function installDependencies(projectName: string) {
+  await $`cd ${projectName} && bun init > /dev/null`;
+  await $`cd ${projectName} && bun install wave-shell --silent`;
+}
+
+function getPackageJson(path: string) {
+  const packageJsonPath = join(process.cwd(), path, "package.json");
+  return require(packageJsonPath);
+}
+
+function injectBinNameOnPackageJson(packageJson: Record<string, any>, projectName: string) {
+  packageJson.bin = {
+    [projectName]: `bin/${projectName}`,
+  };
+  return packageJson;
+}
+
+async function writePackageJson(projectName: string, newPackageJson: Record<string, any>) {
+  const packageJsonPath = join(process.cwd(), projectName, "package.json");
+  Bun.write(join(packageJsonPath), JSON.stringify(newPackageJson, null, 2));
+}
 
 export default {
   description: "Initialize a new project",
@@ -19,43 +84,20 @@ export default {
     };
   },
   run: async ({ args, compileTemplate, prompt, print }) => {
-    const [name] = args.argsArray;
-    let projectName = name;
+    const projectName = await getProjectName(prompt, args);
 
-    if (!name) {
-      const response = await prompt.ask("What is the name of your project?");
+    print.success(`Creating project ${projectName}...`);
+    await createProjectStructure(projectName);
 
-      projectName = response.value
-    }
+    const { bin, command } = await compileTemplates(projectName, compileTemplate);
+    await writeTemplates(projectName, bin, command);
 
-    const filesStructure: FilesStructure = {
-      bin: [projectName],
-      src: {
-        commands: [`${projectName}.ts`],
-      },
-    };
-    createFileStructure(filesStructure);
+    print.info("Installing dependencies...");
+    await installDependencies(projectName);
 
-    const creationTemplatePath = join(__dirname, "src/templates/create/");
-
-    const bin = await compileTemplate(join(creationTemplatePath, "bin.surf"), {
-      projectName,
-    });
-
-    const command = await compileTemplate(
-      join(creationTemplatePath, "hello-world.surf"),
-      {
-        description: "Hello World command",
-      }
-    );
-
-    Bun.write(join(__dirname, "bin", projectName), bin);
-    Bun.write(
-      join(__dirname, "src", "commands", `${projectName}.ts`),
-      command
-    );
+    const newPackageJson = injectBinNameOnPackageJson(getPackageJson(projectName), projectName);
+    await writePackageJson(projectName, newPackageJson);
 
     print.success(`Project ${projectName} created successfully!`);
-
   },
 } as WaveCommand;
